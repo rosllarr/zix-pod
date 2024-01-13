468df50671a1cf5ff63bf3e35341dca5ecc1e79f
me <rosllarr@tutanota.com>
2024-01-13 18:19:53 +0700

    import tailwind css

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
-import { Inter } from 'next/font/google'
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'zix-pod',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
